52fba07fbb32eb285ac2b3e0a6cf91acec5e70da
clardizabal <chris.lardizabal@gmail.com>
2016-05-06 10:18:54 -0700

    fix eslints

--- server/server.js
+++ server/server.js
@@ -1,25 +1,25 @@
 import express from 'express';
 import session from 'express-session';
 import passport from 'passport';
-import flash from 'connect-flash';
-import bodyParser from 'body-parser';
+// import flash from 'connect-flash';
+// import bodyParser from 'body-parser';
 
 const app = express();
 
 const port = process.env.PORT || 8080;
 
 require('babel-core/register')({
-  presets: ['es2015', 'react']
+  presets: ['es2015', 'react'],
 });
 
-//MIDDLEWARE
-//==============================================
-//configure our server with all the middleware and routing
+// MIDDLEWARE
+// ==============================================
+// configure our server with all the middleware and routing
 import middleware from './config/middleware';
 middleware(app, express);
 
-//PASSPORT & ROUTES
-//==============================================
+// PASSPORT & ROUTES
+// ==============================================
 // Passport Configuration
 import config from './config/passport';
 config(app, session, passport);
@@ -30,11 +30,8 @@ import './index';
 import routes from './routes/routes';
 routes(app, express, passport);
 
-//LISTEN
-//==============================================
-app.listen(port, err => {
-  if (err) {
-    return console.log(err);
-  }
-  console.log('Mood.ly is listening on ' + port);
-});
+// LISTEN
+// ==============================================
+console.log(`Mood.ly is listening on ${port}`);
+
+app.listen(port);
